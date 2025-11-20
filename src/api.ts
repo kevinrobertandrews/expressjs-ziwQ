80a8c115c173b64b7b5eb925ac9d77c3a5baa0b9
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2025-11-19 22:12:44 -0500

    fix: broken code

--- src/api.ts
+++ src/api.ts
@@ -11,18 +11,6 @@ app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
-interface Time {
-  iso: string;
-  hour12: string;
-  hour24: string
-  ms: number
-}
-
-interface Temperature {
-  value: string;
-  unit: string;
-  time: string;
-}
 
 interface OpenMeteoResponse {
   current: {
@@ -39,34 +27,33 @@ interface OpenMeteoResponse {
 
 app.get('/ottawa', async (req, res) => {
 
-  const ENDPOINT = "https://api.open-meteo.com/v1/forecast?latitude=45.4112&longitude=-75.6981&daily=sunset,sunrise&current=apparent_temperature&timezone=America%2FNew_York"
+  try {
+    const ENDPOINT = "https://api.open-meteo.com/v1/forecast?latitude=45.4112&longitude=-75.6981&daily=sunset,sunrise&current=apparent_temperature&timezone=America%2FNew_York"
+    const response = await fetch(ENDPOINT);
+    const weather = await response.json() as OpenMeteoResponse;
 
-  const response = await fetch(ENDPOINT);
-
-  const weather = await response.json() as OpenMeteoResponse;
-  console.log({ weather })
-
-  const result = {
-    sunrise: {
-      iso: weather.daily.sunrise[0],
-      hour12: "121212",
-      hour24: weather.daily.sunrise[0].split("T")[1],
-      ms: getHours(weather.daily.sunrise[0])
-    },
-    sunset: {
-      iso: weather.daily.sunset[0],
-      hour12: "121212",
-      hour24: weather.daily.sunset[0].split("T")[1],
-      ms: getHours(weather.daily.sunset[0])
-    },
-    temperature: {
-      value: weather.current.apparent_temperature,
-      unit: "C",
-      time: weather.current.time.split("T")[1]
+    const result = {
+      sunrise: {
+        iso: weather.daily.sunrise[0],
+        time: weather.daily.sunrise[0].split("T")[1],
+        ms: getHours(weather.daily.sunrise[0])
+      },
+      sunset: {
+        iso: weather.daily.sunset[0],
+        time: weather.daily.sunset[0].split("T")[1],
+        ms: getHours(weather.daily.sunset[0])
+      },
+      temperature: {
+        value: weather.current.apparent_temperature,
+        time: weather.current.time.split("T")[1],
+        unit: "C",
+      }
     }
-  }
 
-  if (response.ok) {
-    res.status(200).send({ status: "success", timestamp: new Date().toISOString(), data: result })
+    if (response.ok) {
+      res.status(200).send({ status: "success", timestamp: new Date().toISOString(), data: result })
+    }
+  } catch (err) {
+    throw Error("Unexpected error.");
   }
 })
